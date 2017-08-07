f284694a345e60a97b5a61b9389e00f289584928
apple <1455421519@qq.com>
2017-08-07 16:56:09 +0800

    1.swift基础语法 元组

---
+++ tuple.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+
+
+//元组
+var 课程名 = (day:3,unit:"天",lang:"Swift",ver:"3.0")
+
+课程名.day
+课程名.unit
+课程名.ver
+
+
+//optional
+var addr : String? = "guangzhou"
+
+
+
+
+
